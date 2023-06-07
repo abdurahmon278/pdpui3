c29a2f198cb889d75723f0a735ea54bfd1542e4c
Abdurakhmon <abdurakhmon278@gmail.com>
2023-06-07 19:12:56 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:pdpui3/pages/home_page.dart';
+import 'package:pdpui3/pages/intro_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: IntroPage(),
+      routes: {
+        HomePage.id: (context) => HomePage(),
+        IntroPage.id: (context) => IntroPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  static const String id = 'home_page';
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Welcome to App!",
+          style: TextStyle(
+              color: Colors.redAccent,
+              fontSize: 30,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
